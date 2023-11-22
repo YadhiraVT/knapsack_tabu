02e3b8fdb80de20ca8fd9a23c60c9a5cc44eea3b
YadhiraVT <141872532+YadhiraVT@users.noreply.github.com>
2023-11-22 22:20:58 +0000

    test

--- build/lib/knapsack_tabu/__init__.py
+++ build/lib/knapsack_tabu/__init__.py
@@ -11,10 +11,3 @@ def my_res(x, y):
     """A function that multiply. """
     return x-y
     
-def numeros_fibonacci(n):
-    fibs = []
-    a, b = 0, 1
-    while a < n:
-        fibs.append(a)
-        a, b = b, a+b
-    return fibs
