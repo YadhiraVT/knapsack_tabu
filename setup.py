c3bf3145843798f59d43eefbaaf7cad44380bce1
YadhiraVT <141872532+YadhiraVT@users.noreply.github.com>
2023-11-22 16:02:37 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='minimal',
+setup(name='knapsack_tabu',
       version='0.1',
       description='A Python package for something',
       url='http://github.com/rvf0068/minimal',
       author='Yo Mero',
       author_email='yomero@gmail.com',
       license='MIT',
-      packages=['minimal'],
+      packages=['knapsack_tabu'],
       zip_safe=False)
