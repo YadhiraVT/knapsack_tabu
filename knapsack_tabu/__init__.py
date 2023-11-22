64ba99d7c1fc69c66dd3cd19f298ae060434bf52
IsabelOrozco10 <141699947+IsabelOrozco10@users.noreply.github.com>
2023-11-22 15:54:19 -0600

    first commit

---
+++ knapsack_tabu/__init__.py
@@ -0,0 +1,13 @@
+def my_sum(x, y):
+    """A function that sums. """
+    return x+y
+
+
+def my_mul(x, y):
+    """A function that multiply. """
+    return x*y
+
+def my_res(x, y):
+    """A function that multiply. """
+    return x-y
+    
